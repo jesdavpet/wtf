aa94b5ca1a0d2d5efd64e160c77836fc99cfcc74
Jesse David Peterson <jesdavpet@gmail.com>
2016-12-25 22:35:12 -0500

    NPM initialization, mocha testing dependency installed.

--- wtf.js
+++ wtf.js
@@ -3,48 +3,47 @@
  * @version 0.2
  * Concise type evaluation and comparison in JavaScript.
  */
-var wtf = wtf || ( function ( types ) {
-    'use strict';
+var wtf = wtf || (function (types) {
+    'use strict'
 
     /**
      * Evaluates the type of an object.
      * @param {*} thing - the thing to evaluate the type of
      * @returns {string}
      */
-    function type( thing ) { return Object.prototype.toString.call( thing ); }
+    function type (thing) { return Object.prototype.toString.call(thing) }
 
     /**
      * Adds a custom function, which evaluates equality of other objects.
      * @param {string} name - the name of function to be added
      * @param {*} exemplar - an object of the type to be evaluated
      */
-    function add( name, exemplar ) {
-        var typeString = type( exemplar );
+    function add (name, exemplar) {
+        var typeString = type(exemplar)
         // Add the function to the export object.
-        exports[name] = function ( thing ) {
-            return ( arguments.length === 0 ) ? typeString : type( thing ) === typeString;
-        };
+        exports[name] = function (thing) {
+            return (arguments.length === 0) ? typeString : type(thing) === typeString
+        }
     }
 
     var exports = {
-        'type' : type,
-        'add' : add
-    };
-
-    types = types || {};
-    for ( var t in types ) {
-      if ( types.hasOwnProperty( t ) ) exports.add( t, types[t] );
+        'type': type,
+        'add':  add,
     }
 
-    return exports;
+    types = types || {}
+    for (var t in types) {
+        if (types.hasOwnProperty(t)) exports.add(t, types[t])
+    }
 
-} ) ( {
-    'ARRAY' : [],
-    'ERROR' : new Error(),
-    'FUNCTION' : function () {},
-    'NULL' : null,
-    'NUMBER' : 42,
-    'OBJECT' : {},
-    'STRING' : 'A string.',
-    'UNDEFINED' : undefined
-} );
+    return exports
+}({
+    'ARRAY': [],
+    'ERROR': new Error(),
+    'FUNCTION': function () {},
+    'NULL': null,
+    'NUMBER': 42,
+    'OBJECT': {},
+    'STRING': 'abc',
+    'UNDEFINED': undefined,
+}))
