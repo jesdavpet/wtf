f939504b49ba499e834c94cba1ccf4f6aa43ba6b
Jesse David Peterson <jesdavpet@gmail.com>
2016-12-26 21:22:32 -0500

    Semver bump to 1.0.0, completed README.md, standard formatting / indentation.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Type evaluation and comparison with WTF.JS
+Type evaluation and comparison with Wtf.js
 ==========================================
 
   - Side steps JavaScript's inconsistent `typeof` and `instanceof` operators
@@ -7,33 +7,57 @@ Type evaluation and comparison with WTF.JS
 JavaScript's native `typeof` operator can be awkward, it's one of the language's warts. Take for example `typeof null`, which one would expect to return `"Null"`, but is actually `"Object"`! Unfortunately `instanceof` is even less helpful than `typeof`.
 
 This can lead to unnecessarily complex (and buggy) comparison statements.
-*Enter WTF.JS to the rescue...*
+*Enter Wtf.js to the rescue...*
 
 
-Using WTF.JS type evaluation and predicates
+Using Wtf.js type evaluation and predicates
 -------------------------------------------
 
-Test for an array type: `if (wtf.ARRAY(x)) { ... }`
+Predefined default types will return `true` / `false` indicating if the (optional) argument passed was of that type -- if no argument is passed, the type string for that type is returned instead:
+  - `Wtf.ARRAY([thing])`
+  - `Wtf.FUNCTION([thing])`
+  - `Wtf.NULL([thing])`
+  - `Wtf.NUMBER([thing])`
+  - `Wtf.OBJECT([thing])`
+  - `Wtf.STRING([thing])`
+  - `Wtf.UNDEFINED([thing])`
+
+Therefore, you can use these functions either as predicates or constants.
+
+Test for an array type:
+```
+if (wtf.ARRAY(x)) {
+  // etc.
+}
+```
 
 Display the type of an unknown object for debugging: `console.log(wtf.type(thing))`
 
 Handling a function argument which may be one of a variety of known types:
 ```
 switch (wtf.type(thing)) {
-    case wtf.STRING() : // Handle strings
+    case wtf.STRING() : // Handle string case
         break;
-    case wtf.NUMBER() : // Handle numbers
+    case wtf.NUMBER() : // Handle number case
         break;
-    case wtf.ARRAY() : // Handle arrays
+    case wtf.ARRAY() : // Handle array case
         break;
-    // Etc.
+    // etc.
+}
+```
+
+Since custom objects / classes will evaluate as a Wtf.OBJECT(), the use of `instanceof` to narrow down the object's sub-type base on constructor as a second step.
+```
+if (wtf.OBJECT(x)) {
+  if (x instanceof FancyClass) doFancyThings();
+  // etc.
 }
 ```
 
 
-On **not** using WTF.JS
+On **not** using Wtf.js
 -----------------------
 
 Common libraries such as: jQuery, Underscore or Lodash, etc. include type helper functions that accomplish the same thing -- use those instead.
 
-**NOTE:** *WTF.JS is a reinvention of the wheel for a size optimized -- and, therefore, dependency-free -- JavaScript project. If that's your use case, then enjoy!*
+**NOTE:** *Wtf.js is a reinvention of a very small wheel for a size optimized -- and, therefore, dependency-free -- JavaScript project. If that's your use case, then enjoy!*
--- test/Wtf.test.js
+++ test/Wtf.test.js
@@ -1,52 +1,54 @@
-const { expect, assert } = require('chai')
-const Wtf = require('../src/Wtf.js')
+var expect = require('chai').expect
+var Wtf = require('../src/Wtf.js')
 
-describe('Wtf', () => {
-    const DEFAULT_TYPES = [
-        { name: 'ARRAY',      type: '[object Array]',       exemplar: [] },
-        { name: 'FUNCTION',   type: '[object Function]',    exemplar: (() => {}) },
-        { name: 'NULL',       type: '[object Null]',        exemplar: null },
-        { name: 'NUMBER',     type: '[object Number]',      exemplar: 42 },
-        { name: 'OBJECT',     type: '[object Object]',      exemplar: {} },
-        { name: 'STRING',     type: '[object String]',      exemplar: 'Abc' },
-        { name: 'UNDEFINED',  type: '[object Undefined]',   exemplar: undefined },
-    ]
+describe('Wtf', function () {
+  var DEFAULT_TYPES = [
+    { name: 'ARRAY',      type: '[object Array]',     exemplar: [] },
+    { name: 'FUNCTION',   type: '[object Function]',  exemplar: function () {} },
+    { name: 'NULL',       type: '[object Null]',      exemplar: null },
+    { name: 'NUMBER',     type: '[object Number]',    exemplar: 42 },
+    { name: 'OBJECT',     type: '[object Object]',    exemplar: {} },
+    { name: 'STRING',     type: '[object String]',    exemplar: 'Abc' },
+    { name: 'UNDEFINED',  type: '[object Undefined]', exemplar: undefined },
+  ]
 
-    let wtf = null
-    beforeEach(() => { wtf = new Wtf() })
+  var wtf = null
+  beforeEach(function () { wtf = new Wtf() })
 
-    it('should have add(...) and type(...) methods', () => {
-        expect(wtf).to.have.ownProperty('add')
-        expect(wtf).to.have.ownProperty('type')
+  it('should have add(...) and type(...) methods', function () {
+    expect(wtf).to.have.ownProperty('add')
+    expect(wtf).to.have.ownProperty('type')
+  })
+
+  it('should have common default type comparison methods', function () {
+    DEFAULT_TYPES.forEach(function (t, index, types) {
+      expect(wtf).to.have.ownProperty(t.name)
+      expect(wtf[t.name]()).to.equal(t.type)
+      expect(wtf[t.name](t.exemplar)).to.be.true
+
+      var rotatedIndex = (index+2) % (types.length)
+      var mismatched = types[rotatedIndex]
+      expect(wtf[t.name]()).not.to.equal(mismatched.type)
     })
+  })
 
-    it('should have common default type comparison methods', () => {
-        DEFAULT_TYPES.forEach((t, index, types) => {
-            expect(wtf).to.have.ownProperty(t.name)
-            expect(wtf[t.name]()).to.equal(t.type)
-            expect(wtf[t.name](t.exemplar)).to.be.true
 
-            let rotatedIndex = (index+2) % (types.length)
-            let mismatched = types[rotatedIndex]
-            expect(wtf[t.name]()).not.to.equal(mismatched.type)
-        })
+  describe('.add( ... )', function () {
+    it('should add a comparison function for the type', function () {
+      wtf.add('WTF', wtf)
+
+      expect(wtf).to.have.ownProperty('WTF')
+      expect(wtf.WTF(wtf)).to.be.true
     })
+  })
 
 
-    describe('.add( ... )', () => {
-        it('should add a comparison function for the specified type', () => {
-            wtf.add('WTF', wtf)
-
-            expect(wtf).to.have.ownProperty('WTF')
-            expect(wtf.WTF(wtf)).to.be.true
-        })
+  describe('.type( ... )', function () {
+    it('should return a type string matching checked type', function () {
+      DEFAULT_TYPES.forEach(function (t) {
+        expect(wtf.type(t.exemplar)).to.equal(t.type)
+      })
     })
-
-
-    describe('.type( ... )', () => {
-        it('should return a type string that matches the type being investigated', () => {
-            DEFAULT_TYPES.forEach((t) => { expect(wtf.type(t.exemplar)).to.equal(t.type) })
-        })
-    })
+  })
 
 })
