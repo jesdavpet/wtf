a05dfa8eb9a162f61add756212fe85216a393a71
Jesse David Peterson <jesdavpet@gmail.com>
2017-01-14 20:17:50 -0500

    Refactor module into IIFE, rather than a class.

--- src/Wtf.js
+++ src/Wtf.js
@@ -2,10 +2,15 @@
  * @author Jesse David Peterson <@jesdavpet> (http://www.jes.dav.pet)
  * Concise type evaluation and comparison in JavaScript.
  */
-module.exports = function Wtf () {
+module.exports = (function wtf () {
 'use strict'
 
-  var defaultTypes = {
+  var exports = {
+    add:  add,
+    type: type,
+  }
+
+  var DEFAULT_TYPES = {
     'ARRAY':      [],
     'FUNCTION':   function () {},
     'NULL':       null,
@@ -20,7 +25,7 @@ module.exports = function Wtf () {
    * @param {*} thing - the thing to evaluate the type of
    * @returns {string}
    */
-  this.type = function (thing) {
+  function type (thing) {
     return Object.prototype.toString.call(thing)
   }
 
@@ -29,17 +34,15 @@ module.exports = function Wtf () {
    * @param {string} name - the name of function to be added
    * @param {*} exemplar - an object of the type to be evaluated
    */
-  this.add = function (name, exemplar) {
-    var typeName = this.type(exemplar)
+  function add (name, exemplar) {
+    var typeName = type(exemplar)
     // Add the function to the export object.
-    this[name] = function (thing) {
-      return (arguments.length === 0) ? typeName : this.type(thing) === typeName
+    exports[name] = function (thing) {
+      return (arguments.length === 0) ? typeName : type(thing) === typeName
     }
-
-    return this
   }
 
-  for (var t in defaultTypes) this.add(t, defaultTypes[t])
+    for (var t in DEFAULT_TYPES) exports.add(t, DEFAULT_TYPES[t])
 
-  return this
-}
+  return exports
+}())
